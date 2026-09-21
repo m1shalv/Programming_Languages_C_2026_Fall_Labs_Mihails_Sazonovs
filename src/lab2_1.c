7b5518a10dab8843e48a9e5ee736cbb347e37eea
m1shalv <mishasazonov1998@gmail.com>
2026-09-21 13:11:33 +0000

    Resolve lab2_1 conflict

--- src/lab2_1.c
+++ src/lab2_1.c
@@ -13,16 +13,34 @@
 
 int sum_to_n(int n) {
     // TODO: implement sum with a for loop
+    int sum=0;
+    for(int i = 1; i<=n; i++){
+        sum += i;
+    }
+    return sum;
     return 0; // placeholder
 }
 
 int main(void) {
     int n;
+    while(1){
 
     printf("Enter a positive integer n: ");
-    scanf("%d", &n);
+    if(scanf("%d", &n) != 1){
+        printf("Error: enter the number \n");
+        while (getchar() != '\n');
+        continue;
+    }
 
+    if(n <1){
+        printf("Error: number must be at least 1\n");
+        continue;
+    }
+
+    break;
     // TODO: validate input, call function, and print result
+    }
 
+    printf("Sum of numbers: %d\n", sum_to_n(n));
     return 0;
 }
